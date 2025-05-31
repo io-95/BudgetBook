8fe2c7ae7aec23198d4b156ae40ba373add1f4ed
io-95 <77495768+io-95@users.noreply.github.com>
2025-05-31 15:30:31 +0200

    Print welcome message just once

--- src/BudgetBook.Cli/Program.cs
+++ src/BudgetBook.Cli/Program.cs
@@ -1,11 +1,14 @@
 ﻿int choice = 4; // Default choice is 4 (Exit)
-while(true){
+
+Console.WriteLine("Welcome to the Budget Book CLI!");
+
+while (true)
+{
     choice = PrintStartMenu();
 }
 
 int PrintStartMenu()
 {
-    Console.WriteLine("Welcome to the Budget Book CLI!");
     Console.WriteLine("[1] Add Income, [2] Add Expense, [3] Show Report, [4] Exit");
     Console.Write("Please select an option (standard input is 4): ");
     try
