d9ad8dfddb576a37700e7206bac5aa72c533f497
io-95 <77495768+io-95@users.noreply.github.com>
2025-05-31 15:13:17 +0200

    Implement functions to print the menus

--- src/BudgetBook.Cli/Program.cs
+++ src/BudgetBook.Cli/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int choice;
+choice = PrintStartMenu();
+
+int PrintStartMenu()
+{
+    Console.WriteLine("Welcome to the Budget Book CLI!");
+    Console.WriteLine("[1] Add Income, [2] Add Expense, [3] Show Report, [4] Exit");
+    Console.Write("Please select an optio (standard input is 4): ");
+    int choice = int.Parse(Console.ReadLine() ?? "4");
+
+    return choice;
+}
+
+int PrintReportMenu()
+{
+    Console.WriteLine("Budget Report:");
+    Console.WriteLine("[1] Today's Report, [2] This Month's Report, [3] Back to Main Menu");
+    Console.Write("Please select an optio (standard input is 3): ");
+    int choice = int.Parse(Console.ReadLine() ?? "3");
+
+    return choice;
+}
