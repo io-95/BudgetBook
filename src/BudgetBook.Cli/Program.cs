78ac1162e89d770e7d9aea3d37999d8b387ebb2f
io-95 <77495768+io-95@users.noreply.github.com>
2025-05-31 15:41:17 +0200

    Put the code of the menu printing functions in infinite loops until a proper input is made

--- src/BudgetBook.Cli/Program.cs
+++ src/BudgetBook.Cli/Program.cs
@@ -9,43 +9,51 @@ while (true)
 
 int PrintStartMenu()
 {
-    Console.WriteLine("[1] Add Income, [2] Add Expense, [3] Show Report, [4] Exit");
-    Console.Write("Please select an option (standard input is 4): ");
-    try
+    while (true)
     {
-        choice = int.Parse(Console.ReadLine() ?? "4");
+        Console.WriteLine("[1] Add Income, [2] Add Expense, [3] Show Report, [4] Exit");
+        Console.Write("Please select an option (standard input is 4): ");
+        try
+        {
+            choice = int.Parse(Console.ReadLine() ?? "4");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input. Please enter a number.");
+            continue;
+        }
+        if (choice < 1 || choice > 4)
+        {
+            Console.WriteLine("Invalid choice. Input must be between 1 and 4.");
+            continue;
+        }
+        return choice;
     }
-    catch (FormatException)
-    {
-        Console.WriteLine("Invalid input. Please enter a number.");
-    }
-    if (choice < 1 || choice > 4)
-    {
-        Console.WriteLine("Invalid choice. Input must be between 1 and 4.");
-    }
-
-    return choice;
 }
 
 int PrintReportMenu()
 {
-    Console.WriteLine("Budget Report:");
-    Console.WriteLine("[1] Today's Report, [2] This Month's Report, [3] Back to Main Menu");
-    Console.Write("Please select an option (standard input is 3): ");
-    try
+    while (true)
     {
-        choice = int.Parse(Console.ReadLine() ?? "3");
-    }
-    catch (FormatException)
-    {
-        Console.WriteLine("Invalid input. Please enter a number.");
-    }
-    if (choice < 1 || choice > 3)
-    {
-        Console.WriteLine("Invalid choice. Input must be between 1 and 3");
-    }
-
-    return choice;
+        Console.WriteLine("Budget Report:");
+        Console.WriteLine("[1] Today's Report, [2] This Month's Report, [3] Back to Main Menu");
+        Console.Write("Please select an option (standard input is 3): ");
+        try
+        {
+            choice = int.Parse(Console.ReadLine() ?? "3");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input. Please enter a number.");
+            continue;
+        }
+        if (choice < 1 || choice > 3)
+        {
+            Console.WriteLine("Invalid choice. Input must be between 1 and 3");
+            continue;
+        }
+        return choice;
+    }   
 }
 
 void StartMenuHandler(int input) 
