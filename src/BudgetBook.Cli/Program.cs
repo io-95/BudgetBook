b6cbcf1f9d3c1c132b135c8ae2561e5a26066fbb
io-95 <77495768+io-95@users.noreply.github.com>
2025-05-31 15:19:45 +0200

    Correct spelling mistake in BudgetBook.Cli

--- src/BudgetBook.Cli/Program.cs
+++ src/BudgetBook.Cli/Program.cs
@@ -1,11 +1,14 @@
 ﻿int choice;
-choice = PrintStartMenu();
+while(true){
+    choice = PrintStartMenu();
+}
 
 int PrintStartMenu()
 {
     Console.WriteLine("Welcome to the Budget Book CLI!");
     Console.WriteLine("[1] Add Income, [2] Add Expense, [3] Show Report, [4] Exit");
-    Console.Write("Please select an optio (standard input is 4): ");
+    Console.Write("Please select an option (standard input is 4): ");
+
     int choice = int.Parse(Console.ReadLine() ?? "4");
 
     return choice;
@@ -15,7 +18,7 @@ int PrintReportMenu()
 {
     Console.WriteLine("Budget Report:");
     Console.WriteLine("[1] Today's Report, [2] This Month's Report, [3] Back to Main Menu");
-    Console.Write("Please select an optio (standard input is 3): ");
+    Console.Write("Please select an option (standard input is 3): ");
     int choice = int.Parse(Console.ReadLine() ?? "3");
 
     return choice;
