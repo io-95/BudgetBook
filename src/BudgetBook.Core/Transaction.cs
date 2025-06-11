c129f94672c780dfb0dbda32b90164dc736efb4b
io-95 <77495768+io-95@users.noreply.github.com>
2025-06-11 22:44:02 +0200

    Implement print Mehtod to print a single Transaction

--- src/BudgetBook.Core/Transaction.cs
+++ src/BudgetBook.Core/Transaction.cs
@@ -1,6 +1,35 @@
 ﻿namespace BudgetBook.Core;
 
+public enum TransactionType
+{
+    Income,
+    Expense
+}
 public class Transaction
 {
+    public string Description { get; set; }
+    public decimal Amount { get; set; }
+    public DateTime Date { get; set; }
+    public TransactionType Type { get; set; }
 
+    public Transaction(DateTime date, string description, decimal amount, TransactionType type)
+    {
+        Date = date;
+        Description = description;
+        Amount = amount;
+        Type = type;
+    }
+
+    public void PrintTransaction()
+    {
+        Console.WriteLine($"{Date.ToShortDateString()} | {Description} | ");
+        if (Type == TransactionType.Income)
+        {
+            Console.WriteLine($"+ {Amount:C}");
+        }
+        else
+        {
+            Console.WriteLine($"- {Amount:C}");
+        }
+    }
 }
