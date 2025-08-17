22092cf1a6fc863eba214e82fbfc2f2195130121
io-95 <77495768+io-95@users.noreply.github.com>
2025-08-17 22:10:30 +0200

    Set project reference in BudgetBook.Persistence.csproj to use BudgetBook.Core in PersistenceManager.cs. Subcribe to the event handler TransactionAdded in PeristenceManager.cs

--- src/BudgetBook.Persistence/PersistanceManager.cs
+++ src/BudgetBook.Persistence/PersistanceManager.cs
@@ -1,9 +1,15 @@
-﻿namespace BudgetBook.Persistence;
+﻿using System.Transactions;
+using BudgetBook.Core;
+
+namespace BudgetBook.Persistence;
 
 public class PersistenceManager
 {
     public PersistenceManager()
     {
+        TransactionStore.Instance.TransactionAdded += async (sender, Transaction) =>
+        {
 
+        };
     }
 }
