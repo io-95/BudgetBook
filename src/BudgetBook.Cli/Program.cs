393ab37d217729af937aeaa0f9b24370f10b794f
io-95 <77495768+io-95@users.noreply.github.com>
2025-08-19 23:01:28 +0200

    Import BudgetBook.Persistence and create a PersistenceManager object with using so the dipose method is called automatically on shutdown

--- src/BudgetBook.Cli/Program.cs
+++ src/BudgetBook.Cli/Program.cs
@@ -1,9 +1,14 @@
 ﻿using BudgetBook.Core;
+using BudgetBook.Persistence;
+
+using var persistence = new PersistenceManager();
 
 int choice = 4; // Default choice is 4 (Exit)
 
 TransactionManager transactionManager = new TransactionManager(PrintMenu.PrintReportMenu);
 Console.WriteLine("Welcome to the Budget Book CLI!\n");
+
+
 
 while (true)
 {
